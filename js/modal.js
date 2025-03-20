dee1418eb7db4caabf87b3b92ccfe9e203d792d9
lucasger78 <lucasger78@hotmail.com>
2025-03-20 00:16:14 -0300

    modal video

---
+++ js/modal.js
@@ -0,0 +1,12 @@
+// const video = document.getElementById("modalVideo");
+// const playPauseBtn = document.getElementById("playPauseBtn");
+
+// playPauseBtn.addEventListener("click", () => {
+//     if (video.paused) {
+//         video.play();
+//         playPauseBtn.textContent = "Pausar";
+//     } else {
+//         video.pause();
+//         playPauseBtn.textContent = "Reproducir";
+//     }
+// });
